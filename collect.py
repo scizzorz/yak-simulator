1381b9f261e2560c1ea0535edc964057370f48f0
John Weachock <jweachock@gmail.com>
2016-04-11 16:19:30 -0400

    Improve collector's output

--- collect.py
+++ collect.py
@@ -18,12 +18,12 @@ def get_new_yaks():
     cur_yak = session.query(db.Yak).filter_by(yid=yak.message_id).first()
     if not cur_yak:
       cur_yak = db.Yak(yid=yak.message_id, time=yak.time, text=yak.message, score=yak.number_of_likes, handle=yak.nickname or '')
-      print('NEW:', repr(cur_yak))
+      print(' NEW:', repr(cur_yak))
       session.add(cur_yak)
     else:
       if cur_yak.score != yak.number_of_likes:
+        print('{: =+4}:'.format(yak.number_of_likes - cur_yak.score), repr(cur_yak))
         cur_yak.score = yak.number_of_likes
-        print('MOD:', repr(cur_yak))
 
   session.commit()
 
