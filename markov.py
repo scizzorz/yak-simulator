9b7c924cd01285d32bcd442f2c03f322ac87aa4f
John Weachock <jweachock@gmail.com>
2016-04-11 20:18:03 -0400

    Add similarity detection and minimum length to generator

--- markov.py
+++ markov.py
@@ -1,11 +1,18 @@
+from difflib import SequenceMatcher
 from pymarkovchain import MarkovChain
 import argparse
-import time
 import sys
 
+def similar(a, b):
+  return SequenceMatcher(None, a, b).ratio()
+
 parser = argparse.ArgumentParser(description='Spew some dank memes.')
-parser.add_argument('-n', '--num', type=int, default=1, help='Number of memes to spit. 0 is infinite.')
-parser.add_argument('-t', '--time', type=int, default=10, help='Interval to rip memes when looping.')
+parser.add_argument('-n', '--num', type=int, default=1,
+                    help='Number of memes to spit. 0 is infinite.')
+parser.add_argument('-s', '--similarity', type=float, default=1.0,
+                    help='Reject memes until they\'re at most this dank.')
+parser.add_argument('-l', '--length', type=int, default=0,
+                    help='Reject memes until they\'re at least this long.')
 
 args = parser.parse_args()
 
@@ -13,14 +20,32 @@ def mkdb():
   mc = MarkovChain('./markov')
   with open('yaks.txt') as input:
     mc.generateDatabase(input.read())
-  return mc
+  with open('yaks.txt') as input:
+    yaks = [l.strip() for l in input]
+  return mc, yaks
+
+def mkstr(mc, yaks):
+  res = mc.generateString()
+  sim = 0.0
+  for yak in yaks:
+    new_sim = similar(res.split(), yak.split())
+    if new_sim > sim:
+      sim = new_sim
+
+  return res, sim
 
 if args.num > 0:
-  mc = mkdb()
+  mc, yaks = mkdb()
   for x in range(args.num):
-    print(mc.generateString())
+    sim = args.similarity
+    yak = ''
+    while sim >= args.similarity or len(yak.split()) < args.length:
+      yak, sim = mkstr(mc, yaks)
+    print('{:.2f} {}'.format(sim, yak))
 else:
   while True:
-    mc = mkdb()
-    print(mc.generateString())
-    time.sleep(args.time)
+    mc, yaks = mkdb()
+    sim = args.similarity
+    while sim >= args.similarity or len(yak.split()) < args.length:
+      yak, sim = mkstr(mc, yaks)
+    print('{:.2f} {}'.format(sim, yak))
