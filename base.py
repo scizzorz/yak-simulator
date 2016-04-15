a6d12fd86a684d27e66c2de181cf81c991117dc5
John Weachock <jweachock@gmail.com>
2016-04-15 13:05:48 -0400

    Ask user for PIN if they don't have an ID

--- base.py
+++ base.py
@@ -7,7 +7,12 @@ with open('config.json') as fd:
   config = json.loads(fd.read())
 
 client = yikyak.YikYak();
-client.login_id(config['country'], config['phone'], config['id'])
+if 'id' in config:
+  client.login_id(config['country'], config['phone'], config['id'])
+else:
+  pin = input('PIN? ')
+  client.login(config['country'], config['phone'], pin)
+  print('Your user ID is:', client.yakker.userID)
 
 engine = db.create_engine(config['db'])
 session = db.create_session(engine)
